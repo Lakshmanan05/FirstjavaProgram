5a2f06c8244f9cf42c4a63805e051a5b8d408f3c
Laksh05 <lakshmani468@gmail.com>
2023-05-01 14:23:57 +0530

    changed a to 75

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String [] arg){
-        int a=70;
+        int a=75;
         int b=33;
         int c=a+b;
         System.out.println("addition "+c);
