235607241f7fafe372079c42383ccea96032c58c
Lakshmanan05 <113253881+Lakshmanan05@users.noreply.github.com>
2023-05-01 14:12:48 +0530

    variable a to 70

--- First.java
+++ First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String [] arg){
-        int a=67;
+        int a=70;
         int b=33;
         int c=a+b;
         System.out.println("addition "+c);
